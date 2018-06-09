bb771a089efb695f357dc27894e9f54e8274a90b
Daniel Eros <drerosdaniel@gmail.com>
2018-06-09 20:19:48 +0100

    playing with ajax

--- Vidly/Controllers/CustomersController.cs
+++ Vidly/Controllers/CustomersController.cs
@@ -75,8 +75,50 @@ namespace Vidly.Controllers
             return RedirectToAction("Index", "Customers");
         }
 
+        public ActionResult GetCustomerFormViewModel()
+        {
+            CustomerFormViewModel cfvm = new CustomerFormViewModel();
+            Customer customer = _context.Customers.Include(x => x.MembershipType).SingleOrDefault(x => x.Id == 1);
+            List < MembershipType > membershipTypes = _context.MembershipTypes.ToList();
+            cfvm.Customer = customer;
+            cfvm.MembershipTypes = membershipTypes;
+            return Json(cfvm, JsonRequestBehavior.AllowGet);
+        }
 
-        [Route("Customers/Details/{id}")]
+        [HttpPost]
+        //[ValidateAntiForgeryToken]
+        public ActionResult SavePartial(CustomerFormViewModel ncvm)
+        {
+            if (!ModelState.IsValid)
+            {
+                List<MembershipType> membershipTypes = _context.MembershipTypes.ToList();
+                ncvm.MembershipTypes = membershipTypes;
+                return PartialView("_CustomerFormPartial", ncvm);
+            }
+            if (ncvm.Customer.Id == 0)
+            {
+                _context.Customers.Add(ncvm.Customer);
+            }
+            else
+            {
+                Customer customerInDb = _context.Customers.Single(x => x.Id == ncvm.Customer.Id);
+
+                // Automapperrel lehetne így:
+                // Mapper.Map(ncvm.Customer, customerInDb);
+                // de ekkor MINDEN mezőt felülírna, meg lehet oldani ezt is, de most nem fogl.
+
+                customerInDb.Name = ncvm.Customer.Name;
+                customerInDb.Birthday = ncvm.Customer.Birthday;
+                customerInDb.MembershipTypeId = ncvm.Customer.MembershipTypeId;
+                customerInDb.IsSubsrcibedToNewsletter = ncvm.Customer.IsSubsrcibedToNewsletter;
+            }
+
+            _context.SaveChanges();
+            return RedirectToAction("Index", "Customers");
+        }
+
+
+
         public ActionResult Details(int id)
         {
             // with eager loading we can access the attached tables
@@ -101,6 +143,32 @@ namespace Vidly.Controllers
             return View(cdvm);
         }
 
+        
+        public ActionResult DetailsToDiv(int id)
+        {
+            // with eager loading we can access the attached tables
+            // to do this, use Include(x => x.Membershiptype) in this case
+            // and have to manually import "using System.Data.Entity;"
+            var customer = _context.Customers.Include(x => x.MembershipType).SingleOrDefault(x => x.Id == id);
+            if (customer == null)
+            {
+                return HttpNotFound();
+            }
+
+            // this shoulb be done in a more sopisticated manner
+            //var membershipType = _context.MembershipTypes.SingleOrDefault(x => x.Id == customer.MembershipTypeId);
+
+
+            CustomerDetailViewModel cdvm = new CustomerDetailViewModel
+            {
+                Name = customer.Name,
+                MembershipTypeName = customer.MembershipType.Name,
+                Birthday = customer.Birthday,
+                Id = customer.Id
+            };
+            return PartialView("_DetailsToDivPartial", cdvm);
+        }
+
         public ActionResult Edit(int id)
         {
             var cusromer = _context.Customers.Include(x => x.MembershipType).SingleOrDefault(x => x.Id == id);
@@ -113,6 +181,21 @@ namespace Vidly.Controllers
                 Customer = cusromer, MembershipTypes = _context.MembershipTypes.ToList()
             };
             return View("CustomerForm", ncvm);
+        }
+
+        public ActionResult EditToDiv(int id)
+        {
+            var cusromer = _context.Customers.Include(x => x.MembershipType).SingleOrDefault(x => x.Id == id);
+            if (cusromer == null)
+            {
+                return HttpNotFound();
+            }
+            CustomerFormViewModel ncvm = new CustomerFormViewModel
+            {
+                Customer = cusromer,
+                MembershipTypes = _context.MembershipTypes.ToList()
+            };
+            return PartialView("_CustomerFormPartial", ncvm);
         }
     }
 }
--- Vidly/ViewModels/CustomerDetailViewModel.cs
+++ Vidly/ViewModels/CustomerDetailViewModel.cs
@@ -8,6 +8,7 @@ namespace Vidly.ViewModels
 {
     public class CustomerDetailViewModel
     {
+        public int Id { get; set; }
         public string Name { get; set; }
         public DateTime? Birthday { get; set; }
         public string MembershipTypeName { get; set; }
